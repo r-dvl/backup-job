2e87eb97cbcd557e322f90ce4b83ee7baabe7a1e
r-dvl <rauldel.valle.lima@gmail.com>
2025-03-23 21:45:56 +0100

    Add streamlit UI

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import subprocess
+
+def run_script(arg1, arg2):
+    log_placeholder = st.empty()
+
+    script_args = [arg1, arg2]
+
+    process = subprocess.Popen(
+        ["bash", "backup.sh"] + script_args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        bufsize=1,
+        universal_newlines=True
+    )
+
+    log_output = ""
+    with process.stdout as stdout, process.stderr as stderr:
+        for line in stdout:
+            log_output += line.strip() + "\n"
+            log_placeholder.markdown(f"```bash\n{log_output}```", unsafe_allow_html=True)
+        for line in stderr:
+            log_output += line.strip() + "\n"
+            log_placeholder.markdown(f"```bash\n{log_output}```", unsafe_allow_html=True)
+
+arg1 = st.text_input("Source", "src")
+arg2 = st.text_input("Destination", "backup")
+
+if st.button("Run Backup"):
+    run_script(arg1, arg2)
--- dockerfile
+++ dockerfile
@@ -1,11 +1,27 @@
-FROM alpine:latest
+# Usar una imagen base de Python más ligera
+FROM python:3.11-slim
 
-RUN apk add --no-cache bash curl rsync
+WORKDIR /app
 
-WORKDIR /scripts
+RUN apt-get update && apt-get install -y \
+    bash \
+    curl \
+    rsync \
+    && rm -rf /var/lib/apt/lists/*
 
-COPY src/backup.sh /scripts/backup.sh
+COPY . /app
 
-RUN chmod +x /scripts/backup.sh
+RUN python3 -m venv venv
+ENV PATH="/app/venv/bin:$PATH"
 
-ENTRYPOINT ["/scripts/backup.sh"]
+RUN pip install --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 8080
+
+ENV STREAMLIT_SERVER_HEADLESS=true
+ENV STREAMLIT_SERVER_PORT=8501
+ENV STREAMLIT_SERVER_ENABLECORS=false
+ENV STREAMLIT_BROWSER_GATHER_USAGE_STATS=false
+
+CMD ["streamlit", "run", "app.py", "--server.enableCORS=false", "--server.headless=true", "--server.port=8080"]
